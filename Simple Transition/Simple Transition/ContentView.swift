318cd5d283114bc93a804a48cdc5f16c41c2984d
sothea007 <nemsothea13@gmail.com>
2025-06-30 15:15:44 +0700

    git commit -m "first commit"

---
+++ Simple Transition/Simple Transition/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  Simple Transition
+//
+//  Created by NEMSOTHEA on 6/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var showDetail = false
+    
+    var body: some View {
+        VStack {
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: 300,height: 300)
+                .foregroundStyle(.green)
+                .overlay {
+                    Text("Show detail")
+                        .font(.system(.largeTitle, design: .rounded))
+                        .bold()
+                        .foregroundStyle(.white)
+                        .padding()
+                }
+               
+            if showDetail {
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: 300,height: 300)
+                    .foregroundStyle(.purple)
+                    .overlay {
+                        Text("Well, here is the detail")
+                            .font(.system(.largeTitle, design: .rounded))
+                            .bold()
+                            .foregroundStyle(.white)
+                            .padding()
+                    }
+            }
+            
+        }
+        
+        .onTapGesture {
+            withAnimation(.default) {
+                self.showDetail.toggle()
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Simple Transition/Simple Transition/Simple_TransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_TransitionApp.swift
+//  Simple Transition
+//
+//  Created by NEMSOTHEA on 6/30/25.
+//
+
+import SwiftUI
+
+@main
+struct Simple_TransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
